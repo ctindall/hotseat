6f879d00ad6cc14ea58d43bd043143e66faa9cb9
Cameron Tindall <ctindall@gmail.com>
2018-09-08 17:52:40 -0500

    out with the old, in with the new

--- hotseat_server.pl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env perl
-use strict;
-use warnings; 
-
-use lib 'lib';
-use Mojolicious::Commands;
-
-Mojolicious::Commands->start_app('HotSeat');
--- lib/HotSeat.pm
+++ lib/HotSeat.pm
@@ -1,186 +1,33 @@
 package HotSeat;
-use lib 'lib';
 use Mojo::Base 'Mojolicious';
-use Mojo::Upload;
-use HotSeat::Model::Games;
 
+use HotSeat::Model::Game;
+
+# This method will run once at server start
 sub startup {
-    my $self = shift;
-    
-    $self->secrets(['-1109089652486567240']);
-    $self->plugin('RenderFile');
+  my $self = shift;
+  
+  # Load configuration from hash returned by "my_app.conf"
+  my $config = $self->plugin('Config');
+  
+  # Router
+  my $r = $self->routes;
+  $r->post('/game')
+      ->to('game#create')
+      ->name('create_game');
 
-    my $config = $self->plugin('Config');
-    set_games_dir($config->{games_dir});
-    
-    my $r = $self->routes;
+  $r->post('/game/:game_id')
+      ->to('game#create_existing')
+      ->name('create_game_existing');
 
-    $r->get('/game/:game_id/lock' => sub { 
-	my $c = shift;
-	my $game_id = $c->stash('game_id') + 0; #ensure game_id is a number
-	my %game = get_game($game_id);
+  $r->get('/game/:game_id')
+      ->to('game#read')
+      ->name('read_game');
 
-	if (!$game{'exists'}) {
-	    $c->render(json => {
-		game_id => $game_id,
-		exists => \0,
-		error => "No such game",
-	    }, status => 404);
-	}
-	
-	$c->render(json => {
-	    game_id => $game{'game_id'},
-	    locked => $game{'locked'} ? \1 : \0,
-	    locked_by => $game{'locked_by'},
-	    rom_name => $game{'rom_name'},
-	    system => $game{'system'},
-	});
-    });
-
-    $r->put('/game/:game_id' => sub { 
-	my $c = shift;
-	my $game_id = $c->stash('game_id') + 0; #ensure game_id is a number
-	my $rom_name = $c->param('rom_name');
-	my $system = $c->param('system');
-	my $owned_by = $c->param('owned_by');
-	my $password = $c->param('password');
-	my %game = get_game($game_id);
-	my $success = 0;
-
-	unless (defined $rom_name && defined $system && defined $owned_by && defined $password) {
-	    $c->render(json => {
-		game_id => $game_id,
-		error => "Could not create game: password, owned_by, rom_name, & system are required.",
-	     }, status => 400);
-
-	    return;
-	}
-	
-	if (!$game{'exists'}) {
-	    $success  = create_game $game_id, $rom_name, $system, $owned_by, $password;
-	    %game = get_game $game_id;
-	}
-
-	if ($success) {
-	    $c->render(json => {
-		game_id => $game{'game_id'},
-		locked => $game{'locked'} ? \1 : \0,
-		locked_by => $game{'locked_by'},
-		rom_name => $game{'rom_name'},
-		system => $game{'system'},
-		owned_by => $game{'owned_by'}
-	     }, status => 201);
-
-	    return;
-	}
-	    
-	$c->render(json => {
-	    game_id => $game_id,
-	    error => "Could not create game.",
-	}, status => 400);
-    });
-    
-    $r->post('/game/:game_id/lock' => sub {
-	my $c = shift;
-	my $game_id = $c->stash('game_id') + 0; #ensure game_id is a number
-	my $user = $c->param('user');
-	my $pass = $c->param('password');
-	my %game = get_game($game_id);
-
-	unless ($game{'exists'}) {
-	    $c->render(json => {
-		game_id => $game_id,
-		error => "No such game",	    
-	    }, status => 404);
-
-	    return;
-	}
-
-	unless (defined $pass && $game{'password'} eq $pass) {
-	    $c->render(json => {
-		game_id => $game_id,
-		error => "Bad password.",	    
-	    }, status => 403);
-
-	    return;
-	}
-	
-	unless (defined $user) {
-	    $c->render(json => {
-		game_id => $game_id,
-		error => "User required.",	    
-	    }, status => 400);
-
-	    return;
-	}
-	
-	%game = lock_game $game_id, $user;
-	
-	$c->render(json => {
-	    game_id => $game{'game_id'},
-	    locked => $game{'locked'} ? \1 : \0,
-	    locked_by => $game{'locked_by'},
-	    rom_name => $game{'rom_name'},
-	    system => $game{'system'},
-	    owned_by => $game{'owned_by'}
-        }, status => 201);
-    });
-
-    $r->delete('/game/:game_id/lock' => sub {
-	my $c = shift;
-	my $game_id = $c->stash('game_id') + 0; #ensure game_id is a number
-	my %game = unlock_game($game_id);
-
-	$c->render(json => {
-	    game_id => $game{'game_id'},
-	    locked => $game{'locked'} ? \1 : \0,
-	    locked_by => $game{'locked_by'},
-	    rom_name => $game{'rom_name'},
-	    system => $game{'system'},
-        });
-   });
-
-
-    #savestate upload
-
-    $r->post('/game/:game_id/state' => sub {
-	my $c = shift;
-	my $game_id = $c->stash('game_id') + 0;
-	my $user = $c->param('user');
-	my %game = get_game($game_id);
-
-	if($game{'locked'} && $game{'locked_by'} ne $user) {
-	    $c->render(json => {
-		game_id => $game{'game_id'},
-		locked => $game{'locked'} ? \1 : \0,
-		locked_by => $game{'locked_by'},
-		rom_name => $game{'rom_name'},
-		system => $game{'system'},
-	     }, status => 403);
-
-	    return;
-	}
-	
-	my $state_file = $c->param('save_state');
-	$state_file->move_to(`mktemp`);
-	
-	$c->render(json => {
-	    game_id => $game{'game_id'},
-	    locked => $game{'locked'} ? \1 : \0,
-	    locked_by => $game{'locked_by'},
-	    rom_name => $game{'rom_name'},
-	    system => $game{'system'},
-	});
-    });
-    
-    $r->get('/game/:game_id/state' => sub {
-	my $c = shift;
-	my $game_id = $c->stash('game_id') + 0;
-	my %game = get_game($game_id);
-		
-	$c->render_file( filepath => $game{'state_file'},
-			 filename => "state.sna");
-    });
-};
+  $r->patch('/game/:game_id')
+      ->to('game#update')
+      ->name('update_game');
+      
+}
 
 1;
