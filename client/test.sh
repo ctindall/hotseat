383424d0c9b0f60af6704d058575f382ee0ee281
Cameron Tindall <ctindall@gmail.com>
2018-09-17 10:05:30 -0500

    better-automated integration testing

--- client/test.sh
+++ client/test.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-../server/script/hot_seat daemon &
+#start testing server (conf in ../server/hot_seat.testing.conf)
+../server/script/hot_seat daemon --mode testing &
 
+#wait until server is ready
 while ! curl -s http://localhost:3000/ > /dev/null; do
     sleep 1
-done
+done; echo "testing server ready!"
 
-echo "all ready!";
-
+#save off pid to clean up later
 pid=$(ps fauxww | grep hot_seat\ daemon | grep -v grep | awk '{print $2}')
 
+#run the client tests
 raco test lib/*.rkt
 
+#clean up testing server and testing datadir
 kill $pid
+find /tmp -type d -name games123456 -delete 
